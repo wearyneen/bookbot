2975a073f59ecc9785fcb026167a6d79b02d8965
wearyneen <randomstuff666@gmail.com>
2024-12-26 19:50:38 +0000

    enable reading book

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        return file_contents
+
+
+frankenstein = main()
+
+print(frankenstein)
